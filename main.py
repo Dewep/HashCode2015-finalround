a6ce0887c821159f58aa20cd5f2a3ec3003b719b
Maigret Aurélien <dewep.net@gmail.com>
2015-03-28 10:00:52 +0100

    Base

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+tab = []
+
+
+with open("test_round.in", "r") as f:
+    R, C, H, S = map(int, f.readline().split())
+    print("%s %s %s %s" % (R, C, H, S))
+    for line in f:
+        l = line.replace('\r', '').replace('\n', '')
+        print("[%s]" % l)
+        tab.append(l)
+
+
+print(tab)
+
+
+with open("output.csv", "w") as text_file:
+    for s in tab:
+        print(s, file=text_file)
