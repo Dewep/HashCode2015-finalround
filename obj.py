a21a9cdd1edf0c1b0edfd53516345b4f1780059b
Steven MARTINS <steven.martins.fr@gmail.com>
2015-03-28 14:12:20 +0100

    djis

--- obj.py
+++ obj.py
@@ -9,9 +9,16 @@ class Balloon(object):
         self.movements = []
         self.id = _id
 
-    def move(self, movement): #+1, -1, 0
-        self.movements.append(movement)
-        #deplacer a la case suivante.
+    def move(self, movement):
+        move = 0
+        if self.current_case.up == movement:
+            move = 1
+        if self.current_case.down == movement:
+            move = -1
+        if self.current_case.next == movement:
+            move = 0
+        self.movements.append(move)
+        self.current_case = movement
 
     def __str__(self):
         return "alt(%s),  current(%s), movements(%s)" % (self.alt, self.current_case, " ".join(self.movements))
